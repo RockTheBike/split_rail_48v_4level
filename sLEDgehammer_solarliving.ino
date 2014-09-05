40a56b675cafe715ffa1bde155ecb4a5115c26d3
Mark P Sullivan <mark@RockTheBike.com>
2014-09-05 01:34:46 -0700

    Animate with randomized PWM (working via indexing)
    - Give up on pointer manipulation; just index

--- sLEDgehammer_solarliving.ino
+++ sLEDgehammer_solarliving.ino
@@ -231,7 +231,8 @@ void playGame() {
 
 void circlingAnimation() {
   #define MILLIS_PER_FRAME 500
-  static int frame_index = 0;  // the frame we're heading towards
+  static int old_frame_index;
+  static int new_frame_index = 0;
   static unsigned long time_for_next_frame;
   const int frames[][NUM_LEDS] = {
     { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
@@ -242,31 +243,17 @@ void circlingAnimation() {
     { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
     { 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
     { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 } };
-  const int* old_frame;
-  const int* new_frame = &frames[0][0];
   // advance to (or initialize) the next frame when necessary
   if( time >= time_for_next_frame ) {
     time_for_next_frame =
       ( time_for_next_frame ? time_for_next_frame : time ) + MILLIS_PER_FRAME;
-  frame_index = (frame_index+1) % (sizeof(frames)/sizeof(*frames));
-    Serial.print("incremented frame_index to ");
-    Serial.print(frame_index);
-    Serial.print(" at ");
-    Serial.println(time);
-    old_frame = new_frame;
-    new_frame = &frames[frame_index][0];
+    old_frame_index = new_frame_index;
+    new_frame_index = (new_frame_index+1) % (sizeof(frames)/sizeof(*frames));
   }
   // PWM via picking between frames with increasing probability of later frame
-  int r = rand();
-  /*
-  Serial.print("rand is ");
-  Serial.print(r);
-  Serial.print("; picking ");
-  Serial.println( r < RAND_MAX / MILLIS_PER_FRAME * ( time_for_next_frame - time ) ? "old_frame" : "new_frame" );
-  */
-  const int* frame = r < RAND_MAX / MILLIS_PER_FRAME * ( time_for_next_frame - time ) ? old_frame : new_frame;
+  int frame_index = rand() < RAND_MAX / MILLIS_PER_FRAME * ( time_for_next_frame - time ) ? old_frame_index : new_frame_index;
   for( i=0; i<NUM_LEDS; i++ )
-    ledState[i] = frame[i] ? STATE_ON : STATE_OFF;
+    ledState[i] = frames[frame_index][i] ? STATE_ON : STATE_OFF;
 }
 
 void doBlink(){
