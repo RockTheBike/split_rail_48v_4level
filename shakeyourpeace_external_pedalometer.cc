d5347e57e35c089ac34bf6108a18307cab1d95de
Mark P Sullivan <mark@RockTheBike.com>
2014-09-18 15:00:25 -0700

    Organize thresholds and levels semantically
    - first in test
    - next i'll test the assembled LEDs
    - and then move it all into the arduino code

--- shakeyourpeace_external_pedalometer.cc
+++ shakeyourpeace_external_pedalometer.cc
@@ -3,6 +3,60 @@
 int level( float v, int rail );
 int main( int arcg, char** argv );
 
+
+// real code below
+
+#define NUM_RAILS 2
+#define MAX_LEVELS 8
+#define MAX_LEDS_PER_RAIL 6
+#define NUM_LEDS 8 // Number of LED outputs
+
+#define STATE_OFF 0
+#define STATE_BLINK 1
+#define STATE_BLINKFAST 3
+#define STATE_ON 2
+
+struct leds_for_levels {
+  const int num_thresholds;
+  const float thresholds[MAX_LEVELS-1];
+  // a record has one more level than it has threshold
+  const int leds_for_level[MAX_LEVELS][MAX_LEDS_PER_RAIL];
+} LEDS_FOR_LEVELS[NUM_RAILS] = {
+  {  // plus rail
+    7,
+    { 22.0, 23.5, 24.8, 25.7, 26.7, 27.0, 27.2 },
+    {  // positions:  2 red, 3 green, 1 white
+      { STATE_BLINK,STATE_OFF,  STATE_OFF,STATE_OFF,STATE_OFF,  STATE_OFF },
+      { STATE_ON,   STATE_OFF,  STATE_OFF,STATE_OFF,STATE_OFF,  STATE_OFF },
+      { STATE_ON,   STATE_ON,   STATE_OFF,STATE_OFF,STATE_OFF,  STATE_OFF },
+      { STATE_OFF,  STATE_OFF,  STATE_ON, STATE_OFF,STATE_OFF,  STATE_OFF },
+      { STATE_OFF,  STATE_OFF,  STATE_ON, STATE_ON, STATE_OFF,  STATE_OFF },
+      { STATE_OFF,  STATE_OFF,  STATE_ON, STATE_ON, STATE_ON,   STATE_OFF },
+      { STATE_OFF,  STATE_OFF,  STATE_ON, STATE_ON, STATE_ON,   STATE_ON },
+      { STATE_OFF,  STATE_OFF,  STATE_ON, STATE_ON, STATE_ON,   STATE_BLINK } }
+  }, {  // minus rail
+    3,
+    { 14.0, 16.0, 23.0 },
+    {  // positions:  1 red, 1 green
+      { STATE_BLINK,  STATE_OFF   },
+      { STATE_ON,     STATE_OFF   },
+      { STATE_OFF,    STATE_ON    },
+      { STATE_OFF,    STATE_BLINK } } } };
+int i = 0;
+
+
+int level( float v, struct leds_for_levels* leds_for_levels ) {
+  // TODO fade in the top (or the next) segment of the mercury via PWM
+  for( i=0; i<leds_for_levels->num_thresholds; i++ )
+    if( v < leds_for_levels->thresholds[i] ) return i;
+  return i;
+}
+
+
+// real code above
+// test infrastructure stuff below
+
+
 struct test_case {
   int rail;
   float v;
@@ -20,8 +74,8 @@ struct test_case test_cases[] = {
   { 1, 13.5, 0 },
   { 1, 15.0, 1 },
   { 1, 20.0, 2 },
-  { 1, 22.5, 3 },
-  { 1, 24.0, 7 } };
+  { 1, 22.5, 2 },
+  { 1, 24.0, 3 } };
 
 int main( int arcg, char** argv ) {
   int failures = 0;
@@ -29,7 +83,7 @@ int main( int arcg, char** argv ) {
   for( int i=0;
     i<sizeof(test_cases)/sizeof(*test_cases);
     test_case++, i++ ) {
-    int actual_level = level( test_case->v, test_case->rail );
+    int actual_level = level( test_case->v, &LEDS_FOR_LEVELS[test_case->rail] );
     printf( "ideal level %d %s actual level %d <= rail=%d, v=%f\n",
       test_case->ideal_level, test_case->ideal_level==actual_level ? "=" : "!=", actual_level,
       test_case->rail, test_case->v );
@@ -40,42 +94,4 @@ int main( int arcg, char** argv ) {
     return 1;
   } else
     return 0;
-}
-
-
-// test infrastructure stuff above
-// real code below
-
-
-#define NUM_RAILS 2
-#define NUM_LEDS 8 // Number of LED outputs
-
-#define STATE_OFF 0
-#define STATE_BLINK 1
-#define STATE_BLINKFAST 3
-#define STATE_ON 2
-
-#define NUM_THRESHOLDS 7
-const float levelVolts[NUM_RAILS][NUM_THRESHOLDS] = {
-  { 22.0, 23.5, 24.8, 25.7, 26.7, 27.0, 27.2 },
-  { 14.0, 16.0, 22.0, 23.0, 23.0, 23.0, 23.0 } };
-// a visually compact table of whether each led should be on/blinking vs off
-// positions:  big pedalometer: 2 red, 3 green, 1 white,  side lights: 1 red, 1 green
-const int LEDS_FOR_LEVEL[NUM_THRESHOLDS+1][NUM_LEDS] = {
-  { STATE_BLINK,STATE_OFF,   STATE_OFF,  STATE_OFF,  STATE_OFF,   STATE_OFF,          STATE_BLINK, STATE_OFF   },
-  { STATE_ON,   STATE_OFF,   STATE_OFF,  STATE_OFF,  STATE_OFF,   STATE_OFF,          STATE_ON,    STATE_OFF   },
-  { STATE_ON,   STATE_ON,    STATE_OFF,  STATE_OFF,  STATE_OFF,   STATE_OFF,          STATE_OFF,   STATE_ON    },
-  { STATE_OFF,  STATE_OFF,   STATE_ON,   STATE_OFF,  STATE_OFF,   STATE_OFF,          STATE_OFF,   STATE_ON    },
-  { STATE_OFF,  STATE_OFF,   STATE_ON,   STATE_ON,   STATE_OFF,   STATE_OFF,          STATE_OFF,   STATE_ON    },
-  { STATE_OFF,  STATE_OFF,   STATE_ON,   STATE_ON,   STATE_ON,    STATE_OFF,          STATE_OFF,   STATE_ON    },
-  { STATE_OFF,  STATE_OFF,   STATE_ON,   STATE_ON,   STATE_ON,    STATE_ON,           STATE_OFF,   STATE_BLINK },
-  { STATE_OFF,  STATE_OFF,   STATE_ON,   STATE_ON,   STATE_ON,    STATE_BLINK,        STATE_OFF,   STATE_BLINK } };
-int i = 0;
-
-
-int level( float v, int rail ) {
-  // TODO fade in the top (or the next) segment of the mercury via PWM
-  for( i=0; i<NUM_THRESHOLDS; i++ )
-    if( v < levelVolts[rail][i] ) return i;
-  return NUM_THRESHOLDS;
 }
